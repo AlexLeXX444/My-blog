e0e79c44a3e513939ab1253dea0098428cd7a9eb
Александр Гарейнов <alexlexx444@yandex.ru>
2023-04-16 17:10:37 +0500

    добавил страницу регистрации

--- Blog/src/main/java/com/app/blog/controllers/PageController.java
+++ Blog/src/main/java/com/app/blog/controllers/PageController.java
@@ -1,18 +1,49 @@
 package com.app.blog.controllers;
 
+import com.app.blog.models.User;
+import com.app.blog.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class PageController {
+    private final UserService userService;
 
     @GetMapping("/")
     public String indexPage() {
         return "index";
     }
 
-    @GetMapping("/user_login")
+    @GetMapping("/user/login")
     public String userLoginPage() {
         return "users/user_login";
+    }
+
+    @GetMapping("/user/registration")
+    public String userRegistrationPage(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "users/user_registration";
+    }
+
+    @PostMapping("/user/registration")
+    public String userRegistration(@ModelAttribute("user") User user) {
+        return switch (userService.registerNew(user)) {
+            case (0) -> "redirect:/user/success_register";
+            case (1) -> "redirect:/user/error_login";
+            case (2) -> "redirect:/user/error_password";
+            case (3) -> "redirect:/user/error_email";
+            default -> "redirect:/";
+        };
+    }
+
+    @GetMapping("/user/registration/success_register")
+    public String userRegistrationSuccessPage() {
+        return "users/success";
     }
 }
