9cec89fa3ae3be0d1e89dd3322143527a3c71fe2
Александр Гарейнов <alexlexx444@yandex.ru>
2023-04-17 20:49:45 +0500

    добавил функционал добавления статьи

--- Blog/src/main/java/com/app/blog/controllers/PostController.java
+++ Blog/src/main/java/com/app/blog/controllers/PostController.java
@@ -1,9 +1,13 @@
 package com.app.blog.controllers;
 
+import com.app.blog.models.Post;
 import com.app.blog.services.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -12,7 +16,15 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/blog/post/add")
-    public String postAddPage() {
-        return "post_add";
+    public String postAddPage(Model model) {
+        Post post = new Post();
+        model.addAttribute("post", post);
+        return "posts/post_add";
+    }
+
+    @PostMapping("/blog/post/add")
+    public String postAdd(@ModelAttribute("post") Post post) {
+        postService.addNewPost(post);
+        return "redirect:/";
     }
 }
